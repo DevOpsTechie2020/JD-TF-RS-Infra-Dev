f5a75333e092600ef4f18542bafeee003b1868e0
Pavan Bokka <rampavan.bokka@gmail.com>
2026-03-11 15:50:06 +0530

    code added

--- 40-Databases/variables.tf
+++ 40-Databases/variables.tf
@@ -5,3 +5,11 @@ variable "project" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+  default = Z08877911SZ25R8IE57OQ
+}
+
+variable "domain_name" {
+  default = "dive2devops.com"
+}
